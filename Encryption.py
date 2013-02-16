fc73248993d173776145814ddf8897399410873b
Glenn T Norton <glenn at year47 dot com>
2013-02-16 00:58:08 -0600

    Added example and unit test

--- Encryption.py
+++ Encryption.py
@@ -28,11 +28,13 @@
 #####
 
 from Crypto.Cipher import AES
+from Crypto import Random
+
 class Encryption(object):
-    def __init__(self, passkey=None, mode=None):
+    def __init__(self, passkey=None, mode=None, iv=None):
         self.passkey = passkey
 	self.mode = mode
-        self.aes = None
+        self.iv = iv
     
     def setPasskey(self, p):
         self.passkey = p
@@ -40,16 +42,64 @@ class Encryption(object):
     def setMode(self, m):
         self.mode = m
 
+    def setIV(self, iv):
+        self.iv = iv
+
     def encrypt(self, s):
         if not self.passkey: raise ValueError('Passkey not set')
         if not self.mode: self.mode = AES.MODE_CFB
+        if not self.iv: self.iv = Random.new().read(AES.block_size)
 
-        self.aes = AES.new(self.passkey, self.mode)
+        self.aes = AES.new(self.passkey, self.mode, self.iv)
         return self.aes.encrypt(s)
 
     def decrypt(self, s):
         if not self.passkey: raise ValueError('Passkey not set')
         if not self.mode: self.mode = AES.MODE_CFB
+        if not self.iv: self.iv = Random.new().read(AES.block_size)
 
-        self.aes = AES.new(self.passkey, self.mode)
+        self.aes = AES.new(self.passkey, self.mode, self.iv)
         return self.aes.decrypt(s)
+
+
+##########################
+# EXAMPLE
+# >>> import Encryption
+# >>> passkey = b'1234567890ABCDEF'
+# >>> s = b'This is a string'
+# >>> e = Encryption.Encryption(passkey=passkey)
+# >>> enc = e.encrypt(s)
+# >>> print enc
+# >>> enc
+# '\xcf\x90\xf7\xfd\x08\x02*T\x97\x1a (\xe3J\x17\xfb'
+# >>> ss = e.decrypt(enc)
+# >>> ss
+# 'This is a string'
+# >>> 
+##########################
+
+import unittest
+class TestEncryption(unittest.TestCase):
+    
+    def setUp(self):
+        self.passkey = b'0123456789ABCDEF'
+        self.s = b'This is a string'
+        self.e = Encryption(self.passkey)
+
+    def testEncrypt(self):
+        self.enc = self.e.encrypt(self.s)
+        self.assertTrue(self.enc != self.s)
+
+    def testDecrypt(self):
+        self.enc = self.e.encrypt(self.s)
+        self.ss = self.e.decrypt(self.enc)
+        self.assertTrue(self.ss == self.s)
+
+    def tearDown(self):
+        del self.passkey
+        del self.s
+        del self.e
+
+if __name__ == '__main__':
+    unittest.main()
+
